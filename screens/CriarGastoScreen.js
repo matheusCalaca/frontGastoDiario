9597e24d29a82a0146de058f2a5b7123f3f595a5
matheus calaça <matheusfcalaca@gmail.com>
2024-03-22 19:55:50 -0300

    adicionado calendario ao projeto

--- screens/CriarGastoScreen.js
+++ screens/CriarGastoScreen.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import axios from 'axios';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
-const CriarGastoScreen = () => {
+
+const CriarGastoScreen = ({ navigation }) => {
     const [userId, setUserId] = useState('');
     const [nome, setNome] = useState('');
     const [valor, setValor] = useState('');
-    const [dataCompra, setDataCompra] = useState('');
+    const [dataCompra, setDataCompra] = useState(new Date());
     const [categoriaId, setCategoriaId] = useState('');
+    const [showDataCompra, setShowDataCompra] = useState(false);
 
     const criarGasto = async () => {
         try {
@@ -15,18 +21,12 @@ const CriarGastoScreen = () => {
                 userId: userId,
                 nome: nome,
                 valor: parseFloat(valor), // Converta o valor para um número
-                dataCompra: dataCompra,
+                dataCompra: convertDate(dataCompra),
                 categoriaId: categoriaId
             }
             console.log("tentativa de criação " + gastoDto.nome + " " + gastoDto.valor + " " + gastoDto.dataCompra + " " + gastoDto.categoriaId + " ");
             // Realize a solicitação HTTP POST para o endpoint 'gasto'
-            const response = await axios.post('http://192.168.5.241:8080/gasto', {
-                userId: userId,
-                nome: nome,
-                valor: parseFloat(valor), // Converta o valor para um número
-                dataCompra: dataCompra,
-                categoriaId: categoriaId
-            });
+            const response = await axios.post('http://192.168.5.241:8080/gasto', gastoDto);
 
             // Verifique a resposta da solicitação
             if (response.status === 201) {
@@ -37,6 +37,7 @@ const CriarGastoScreen = () => {
                 setDataCompra('');
                 setCategoriaId('');
                 alert('Gasto criado com sucesso!');
+                navigation.navigate('Gastos');
             } else {
                 // Se houver um erro na solicitação, exiba uma mensagem de erro
                 alert('Erro ao criar gasto. Tente novamente mais tarde.');
@@ -47,6 +48,41 @@ const CriarGastoScreen = () => {
         }
     };
 
+    const showDatepicker = () => {
+        setShowDataCompra(true);
+    };
+
+    const onChangeDate = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShowDataCompra(false);
+        setDataCompra(currentDate);
+    };
+
+    const convertDate = (dataOriginal) => {
+
+        const ano = dataOriginal.getFullYear();
+        const mes = String(dataOriginal.getMonth() + 1).padStart(2, '0');
+        const dia = String(dataOriginal.getDate()).padStart(2, '0');
+
+        const dataFormatada = `${ano}-${mes}-${dia}`;
+
+        console.log(dataFormatada);
+
+        return dataFormatada;
+    }
+
+    const convertDateBR = (dataOriginal) => {
+
+        const ano = dataOriginal.getFullYear();
+        const mes = String(dataOriginal.getMonth() + 1).padStart(2, '0');
+        const dia = String(dataOriginal.getDate()).padStart(2, '0');
+
+        const dataFormatada = `${dia}/${mes}/${ano}`;
+
+        console.log(dataFormatada);
+
+        return dataFormatada;
+    }
 
     return (
         <View style={styles.container}>
@@ -68,12 +104,24 @@ const CriarGastoScreen = () => {
                 value={valor}
                 onChangeText={setValor}
             />
-            <TextInput
-                style={styles.input}
-                placeholder="Data da compra (AAAA-MM-DD)"
-                value={dataCompra}
-                onChangeText={setDataCompra}
-            />
+            <TouchableOpacity onPressIn={showDatepicker}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Data da compra (AAAA-MM-DD)"
+                    value={convertDateBR(dataCompra)}
+                    editable={false}
+                />
+                <FontAwesomeIcon icon={faCalendar} style={styles.icon} />
+            </TouchableOpacity>
+            {showDataCompra && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={dataCompra}
+                    mode="date"
+                    is24Hour={true}
+                    onChange={onChangeDate}
+                />
+            )}
             <TextInput
                 style={styles.input}
                 placeholder="ID da categoria"
@@ -96,6 +144,13 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#ccc',
         padding: 10,
+    },
+    icon: {
+        position: 'absolute',
+        top: 15,
+        right: 15,
+        zIndex: 1,
+        color: '#888', // Cor do ícone
     },
 });
 
